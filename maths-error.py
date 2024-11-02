ff89d854ebfb2f1aed9a805b671fce7ad1a41d5a
Nathan Sharp <dev@njsharp.uk>
2024-11-02 16:46:29 +0000

    Added a demo maths error

---
+++ maths-error.py
@@ -0,0 +1,10 @@
+# Set the "ans" variable to 0
+ans = 0
+
+# Add "0.1" to "0.2"
+ans = 0.1 + 0.2
+
+# Show the answer in the terminal, the expected answer should be 0.3
+print(ans)
+
+# Notice how the answer you get is wrong.
