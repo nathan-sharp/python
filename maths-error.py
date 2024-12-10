e41e4835def7a47fff8fb0cb4959a40313eaf013
Nathan Sharp <nath20an06@gmail.com>
2024-12-10 20:17:09 +0000

    isupper

--- maths-error.py
+++ maths-error.py
@@ -1,10 +1,11 @@
+
 # Set the "ans" variable to 0
-ans = 0
+ANS = 0
 
 # Add "0.1" to "0.2"
-ans = 0.1 + 0.2
+ANS = 0.1 + 0.2
 
 # Show the answer in the terminal, the expected answer should be 0.3
-print(ans)
+print(ANS)
 
 # Notice how the answer you get is wrong.
